0fbacc306b20af459a33db1cac63cba3c1e59c92
MAD71241 <michele.deliso@gmail.com>
2021-05-10 15:55:36 +0200

    completo snack 2

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ var zucchine = [
     {
         varietà: "Zucchino giallo Golden",
         peso: 0.6,
-        lunghezza: 10
+        lunghezza: 14
 
     }
     ,
@@ -55,21 +55,21 @@ var zucchine = [
     {
         varietà: "Romanesco",
         peso: 0.7,
-        lunghezza: 12
+        lunghezza: 15
 
     }
     ,
     {
         varietà: "Striato di Napoli",
         peso: 0.3,
-        lunghezza: 12
+        lunghezza: 16
 
     }
     ,
     {
         varietà: "Siciliano",
         peso: 1.1,
-        lunghezza: 12
+        lunghezza: 18
 
     }
 ]
@@ -81,3 +81,34 @@ for (var i = 0; i < zucchine.length; i++) {
     console.log(zucchineWeight);
 }
 console.log("il peso totale delle Zucchine è: " + Math.round(zucchineWeight * 100) / 100);
+
+/* Snack 2:
+Crea 10 oggetti che rappresentano una zucchina.
+Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+ */
+var shorterZucchine = [];
+var longerZucchine = [];
+
+for (var i = 0; i < zucchine.length; i++) {
+    var zucchineEl = zucchine[i];
+    if (zucchineEl.lunghezza < 15) {
+        shorterZucchine.push(zucchineEl);
+    } else {
+        longerZucchine.push(zucchineEl)
+    }
+}
+console.log(shorterZucchine);
+console.log(longerZucchine);
+var shorterWeight = 0;
+var longerWeight = 0;
+
+for (var i = 0; i < shorterZucchine.length; i++) {
+    shorterWeight += shorterZucchine[i].peso;
+}
+console.log("il peso delle zucchine più corte è: "+ shorterWeight);
+
+for (var i = 0; i < longerZucchine.length; i++) {
+    longerWeight += longerZucchine[i].peso;
+}
+console.log("il peso delle zucchine più lunghe è: "+ longerWeight);
